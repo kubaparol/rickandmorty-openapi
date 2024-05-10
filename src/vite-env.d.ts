4b37898100aaedaf739e99f5f83fc5698e380cc7
Kuba Parol <99417488+kubaparol@users.noreply.github.com>
2024-05-10 18:25:02 +0200

    ft: add workflows reusability (#4)
    * ft: add workflows reusability
    
    * ft: add EnvironmentInfo component to CharactersContainer

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,13 @@
 /// <reference types="vite/client" />
 
-declare const APP_VERSION: string;
+// en: Extending the import.meta.env object with a new environment variable
+interface ImportMetaEnv {
+  readonly VITE_ENVIRONMENT: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+// Global variable exposed by the "define" configuration
+declare const __APP_VERSION__: string;
